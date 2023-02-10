b44a9d3dcaedc2cf4ed2853b53b6f7bbd63d22ff
Phyllip Hall <phall776@gmail.com>
2023-02-10 08:18:47 -0500

    feat: support for twilio sms callbacks

--- routes/routesMsg.js
+++ routes/routesMsg.js
@@ -1,12 +1,15 @@
 const Router = require('koa-router');
+const { koaBody } = require('koa-body');
 
 const { sendText } = require('./routesMsgText.js');
+const { sendTextStatus } = require('./routesMsgTextStatus.js');
 const { sendEmail } = require('./routesMsgEmail.js');
 const { sendEmailAlert } = require('./routesMsgEmailCmSsrAlert.js');
 
 const msgRouter = new Router({ prefix: '/send' });
 
 msgRouter.all('/text', async (ctx) => { await sendText(ctx); });
+msgRouter.all('/text_status', async (ctx) => { await sendTextStatus(ctx); });
 msgRouter.all('/email', async (ctx) => { await sendEmail(ctx); });
 msgRouter.all('/email_alert', async (ctx) => { await sendEmailAlert(ctx); });
 
