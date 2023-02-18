ecd71b1f65937afc77176c3cda47438ada7b3b93
Phyllip Hall <phall776@gmail.com>
2023-02-18 12:33:34 -0500

    feat: added AWS S3

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,11 @@ const Router = require('koa-router');
 
 const { espRouter } = require('./routesEsp.js');
 const { msgRouter } = require('./routesMsg.js');
+const { s3Router } = require('./routesS3.js');
 
 const apiRouter = new Router({ prefix: '/api' });
 
-const nestedRoutes = [espRouter, msgRouter];
+const nestedRoutes = [espRouter, msgRouter, s3Router];
 for (var router of nestedRoutes) {
   apiRouter.use(router.routes(), router.allowedMethods());
 }
