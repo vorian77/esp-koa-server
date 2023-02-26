3d9c4500d26de25356d8f478f493788b64a78336
Phyllip Hall <phall776@gmail.com>
2023-02-26 09:28:53 -0500

    refactor: migrate esp connect to node_utilities

--- routes/routesEsp.js
+++ routes/routesEsp.js
@@ -1,8 +1,18 @@
 const Router = require('koa-router');
-const { espConnect } = require('./routesEspFunctions.js');
+const { espConnect } = require('@vorian77/node_utilities');
 
 const espRouter = new Router({ prefix: '/esp' });
 
-espRouter.all('(.*)', async (ctx) => { await espConnect(ctx); });
+espRouter.all('(.*)', async (ctx) => { await esp(ctx); });
 
 exports.espRouter = espRouter;
+
+async function esp(ctx) {
+  const method = ctx.request.method;
+  const prefix = '/esp';
+  const espFunction = ctx.path.slice(ctx.path.indexOf(prefix) + prefix.length + 1);
+  
+  const response = await espConnect(method, espFunction, ctx.query);
+  ctx.status = response.status;
+  ctx.body = response.body;
+}
