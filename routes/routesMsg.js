95a641a474a471a155c8bc6ca5d840f20d6cd373
Phyllip Hall <phall776@gmail.com>
2023-03-07 14:59:14 -0500

    refactor: upldated email alert route

--- routes/routesMsg.js
+++ routes/routesMsg.js
@@ -1,13 +1,13 @@
 const Router = require('koa-router');
 
 const { sendEmail, sendText } = require('./routesMsgFunctions.js');
-const espEmailAlert = require('./routesMsgFunctionsEspEmailAlert.js');
+const sendEmailAlertEsp = require('./routesMsgFunctionsEspEmailAlert.js');
 
 const msgRouter = new Router({ prefix: '/msg' });
 
 msgRouter.post('/send_email', async (ctx) => { await sendEmail(ctx); });
+msgRouter.post('/send_email_alert_esp', async (ctx) => { await sendEmailAlertEsp(ctx); });
 msgRouter.post('/send_text', async (ctx) => { await sendText(ctx); });
-msgRouter.post('/esp_email_alert', async (ctx) => { await espEmailAlert(ctx); });
 
 exports.msgRouter = msgRouter;
 
