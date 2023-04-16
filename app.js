286c751eda50c0e59d9cde45eaaa0c9b0feaafe9
Phyllip Hall <phall776@gmail.com>
2023-04-16 09:01:49 -0400

    feat: detect deployment environment

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-"use strict";
+"use strict"
 
 // resources - koa
-const Koa = require('koa');
-const cors = require('@koa/cors');
-const { koaBody } = require('koa-body');
-const json = require('koa-json');
+const Koa = require("koa")
+const cors = require("@koa/cors")
+const { koaBody } = require("koa-body")
+const json = require("koa-json")
 
 // resources - other
-const fs = require('fs');
-const https = require('https');
-const path = require('path');
-require('dotenv').config({ path: path.join(process.cwd(), '.env')});
+const fs = require("fs")
+const https = require("https")
+const path = require("path")
+require("dotenv").config({ path: path.join(process.cwd(), ".env") })
 
 // resources - application
-const { apiRouter } = require('./routes/routes.js');
-const { logger } = require('./util/logger.js');
+const { apiRouter } = require("./routes/routes.js")
+const { logger } = require("./util/logger.js")
 
 //environment parms
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 // app
-const app = new Koa();
+const app = new Koa()
 app
   .use(handleErrors)
   .use(cors())
@@ -28,24 +28,40 @@ app
   .use(json())
   .use(apiRouter.routes())
   .use(apiRouter.allowedMethods)
-  .listen(PORT, logStart(PORT));
+
+if (process.env.HTTPS_CERT == "DEVELOPMENT") {
+  // https listener - certificates
+  var options = {
+    pfx: fs.readFileSync(process.env.HTTPS_CERT),
+    passphrase: process.env.HTTPS_CERT_PW,
+  }
+
+  // https listener - listener
+  https.createServer(options, app.callback()).listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+  })
+} else {
+  app.listen(PORT, logStart(PORT))
+}
 
 function logStart(port) {
-  const runtimeDir = process.cwd();
-  const startMsg = `ESP-API started - Port: ${port} - Run time directory: ${runtimeDir}\r\n`;
-  logger(startMsg);
-};
+  const runtimeDir = process.cwd()
+  const startMsg = `ESP-API started - Port: ${port} - Run time directory: ${runtimeDir}\r\n`
+  logger(startMsg)
+}
 
 // error handler for all processing within app
+// ctx.throw([status], [msg], [properties]) - future implementation
+// http://man.hubwiz.com/docset/koa.docset/Contents/Resources/Documents/context.html
 async function handleErrors(ctx, next) {
   try {
-    await next();
+    await next()
   } catch (err) {
-    ctx.status = err.status || 500;
-    console.error(err.stack);
-    ctx.body = { 
+    ctx.status = err.status || 500
+    console.error(err.stack)
+    ctx.body = {
       status: ctx.status,
-      message: err.stack 
-    };
+      message: err.stack,
+    }
   }
-};
+}
