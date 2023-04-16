8ec47afedb5c486bd1799d7b62fa799ee6cedc9d
Phyllip Hall <phall776@gmail.com>
2023-04-16 09:02:50 -0400

    feat: updated email alert - added site status

--- routes/routesMsgFunctionsEmailAlertEsp.js
+++ routes/routesMsgFunctionsEmailAlertEsp.js
@@ -1,69 +1,96 @@
-"use strict";
+"use strict"
 
 // ESP Self-Service Registration - Send Email Alert
 
-require('dotenv').config();
-const http = require('../util/http.js');
-const sendEmail = require('../util/msgMail.js');
+require("dotenv").config()
+const http = require("../util/http.js")
+const sendEmail = require("../util/msgMail.js")
 
-module.exports = async function(ctx) {
-  ctx.state.applicantId = ctx.request.body.applicantId;
-  ctx.state.alertType = ctx.request.body.alertType;
-  ctx.state.content = ctx.request.body.content;  // optional - used for message alerts
+module.exports = async function (ctx) {
+  ctx.state.applicantId = ctx.request.body.applicantId
+  ctx.state.alertType = ctx.request.body.alertType
+  ctx.state.content = ctx.request.body.content // optional - used for message alerts
 
-  await getApplicantData(ctx);
-  await setEmailContent(ctx);
-  await sendMsg(ctx);
+  await getApplicantData(ctx)
+  await setEmailContent(ctx)
+  await sendMsg(ctx)
 }
 
-const getApplicantData = async function(ctx) {
-  const url = process.env.ESP_DB_URL + '/esp/ws_cm_ssr_email_alert_data';
-  const method = 'get';
-  ctx.query.applicantId = ctx.state.applicantId;
+const getApplicantData = async function (ctx) {
+  const url = process.env.ESP_DB_URL + "/esp/ws_cm_ssr_email_alert_data"
+  const method = "get"
+  ctx.query.applicantId = ctx.state.applicantId
 
   try {
-    const response = await http(method, url, ctx);
-    ctx.state.applicantName = response.data.applicant;
-    ctx.state.emailToList = response.data.emailAddresses;
+    const response = await http(method, url, ctx)
+    ctx.state.applicantName = response.data.applicant
+    ctx.state.emailToList = response.data.emailAddresses
+    ctx.state.site_current = response.data.site_current
+    ctx.state.site_previous = response.data.site_previous
   } catch (err) {
-    console.error(`getApplicantData error...\nURL: ${url} \nQuery Params: ${JSON.stringify(ctx.query)}`);
-    const newErr = new Error(err.message);
-    newErr.status = err.status;
-    throw newErr;
+    console.error(
+      `getApplicantData error...\nURL: ${url} \nQuery Params: ${JSON.stringify(
+        ctx.query,
+      )}`,
+    )
+    const newErr = new Error(err.message)
+    newErr.status = err.status
+    throw newErr
   }
 }
 
-const setEmailContent = async function(ctx) {
-  const applicantName = ctx.state.applicantName;
-  const alertType = ctx.state.alertType;
-  const content = ctx.state.content;
+const setEmailContent = async function (ctx) {
+  const applicantName = ctx.state.applicantName
+  const alertType = ctx.state.alertType
+  const content = ctx.state.content
+  const siteCurrent = ctx.state.site_current
+  const sitePrevious = ctx.state.site_previous
 
   // email subject
-  ctx.state.emailSubject = 'KidSmart - New Self-Service App Upload - ' + alertType + ' (' + applicantName + ')';
-  
-  // email body
-  let emailBody = '<h3>Please check CaseManager for a new self-service registration app upload:</h3>' + 
-    '<p><b>Applicant:</b> ' + applicantName + '</p>' + 
-    '<p><b>Upload Type:</b> ' + alertType + '</p>' ;
-  if (content) { emailBody += '<p></p><p><b>Message Content: </b>' + content + '</p>' };
-  emailBody = '<!DOCTYPE html> <html> <body>' +  emailBody + '</body> </html>';
+  ctx.state.emailSubject =
+    "KidSmart - New Self-Service App File Update - " +
+    alertType +
+    " (" +
+    applicantName +
+    ")"
 
-  ctx.state.emailBody = emailBody;
+  // email body
+  let emailBody =
+    "<h3>Please check CaseManager for a file modification initiated by the applicant: </h3>" +
+    "<p><b>Applicant:</b> " +
+    applicantName +
+    "</p>" +
+    "<p><b>Upload Type:</b> " +
+    alertType +
+    "</p>" +
+    "<p><b>Current Site:</b> " +
+    siteCurrent +
+    "</p>" +
+    "<p><b>Previous Site:</b> " +
+    sitePrevious +
+    "</p>"
+
+  if (content) {
+    emailBody += "<p></p><p><b>Message Content: </b>" + content + "</p>"
+  }
+  emailBody = "<!DOCTYPE html> <html> <body>" + emailBody + "</body> </html>"
+
+  ctx.state.emailBody = emailBody
 }
 
-const sendMsg = async function(ctx) {
+const sendMsg = async function (ctx) {
   const msg = {
     emailToList: ctx.state.emailToList,
-    emailFrom: 'alerts@TheAppFactory.com',
+    emailFrom: "alerts@TheAppFactory.com",
     emailSubject: ctx.state.emailSubject,
-    emailBody: ctx.state.emailBody
+    emailBody: ctx.state.emailBody,
   }
 
   try {
-    const response = await sendEmail(msg);
-    ctx.body = response.body;
-    ctx.status = response.status;
-  } catch(err) {
-    throw err;
+    const response = await sendEmail(msg)
+    ctx.body = response.body
+    ctx.status = response.status
+  } catch (err) {
+    throw err
   }
 }
